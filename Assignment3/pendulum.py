19d9ebf2141983c1b851189409f4b10aefaa197e
Supreeth Suresh <supreethms1809@gmail.com>
2025-04-15 23:09:49 -0600

    Implement LQR backward and forward recursion in Pendulum class; enhance dynamics simulation and state management. ** Matrix sizes are wrong**

--- Assignment3/pendulum.py
+++ Assignment3/pendulum.py
@@ -1,8 +1,10 @@
-from re import A
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 from dataclasses import dataclass, field
+from pyparsing import C
 from scipy.signal import cont2discrete
+import sympy as sp
 
 @dataclass
 class simData:
@@ -29,21 +31,44 @@ class simData:
 class Pendulum:
     def __init__(self, data: simData):
         self.data = data
-        self.state = data.initial_state
+        self.initial_state = data.initial_state
         self.desired_state = data.desired_state
+        self.state = None
+        self.action = None
+        self.desired_action = np.zeros((1, 1))
         self.time = 0
+        self.time_steps = int(self.data.T / self.data.dt)
         self.history = []
         self.A = np.array([[0, 1, 0, 0],
                               [0, -self.data.d/self.data.M, (self.data.b*self.data.m*self.data.g)/self.data.M, 0],
                               [0, 0, 0, 1],
                               [0, (-self.data.b*self.data.d)/self.data.M*self.data.L, (-self.data.b*(self.data.M+self.data.m))*self.data.g/(self.data.M * self.data.L), 0]])
         self.B = np.array([[0], [1/self.data.M], [0], [self.data.b/(self.data.L*self.data.M)]])
+
+        self.Q  = np.eye(4)  # State cost matrix
+        self.R = np.eye(1)
+
+        self.output = {
+            'time': [],
+            'Q_t': [],
+            'q_t': [],
+            'Qofx_tu_t': [],
+            'u_t': [],
+            'K_t': [],
+            'k_t': [],
+            'V_t': [],
+            'v_t': [],
+            'Vofx_t': []
+        }
+
         
 
+    # convert continuous-time system to discrete-time system
     def c2d(self):
         A_d, B_d, _, _, _ = cont2discrete((self.A, self.B,None,None), self.data.dt)
         return A_d, B_d
 
+    # Dynamics function - non-linear
     def non_linear_dynamics(self, state, action):
         x1, x2, x3, x4 = state.flatten()
         x,v,theta,w = state.flatten()
@@ -59,25 +84,37 @@ class Pendulum:
                     (m*L*np.cos(theta)*u))/(m*(L**2)*(M+m*(1-(np.cos(theta)**2))))
         return np.array([[dx1], [dx2.item()], [dx3], [dx4.item()]])
     
+    # Dynamics function - linear
     def lin_dynamics(self, state, action):
         return self.A @ state + self.B @ action
     
-    def simulate(self):
+    # simulate the pendulum dynamics with either linear or non-linear dynamics
+    def simulate(self, dynamics="linear", output=None):
+        if dynamics == "linear":
+            self.dynamics = self.lin_dynamics
+        elif dynamics == "non-linear":
+            self.dynamics = self.non_linear_dynamics
+        else:
+            raise ValueError("Dynamics must be 'linear' or 'non-linear'")
+        
+        self.reset()  
         # Simulate the pendulum dynamics
-        time_steps = int(self.data.T / self.data.dt)
-        for t in range(time_steps):
+        for t in range(self.time_steps):
             #print(f"Timestep t: {t} Time: {self.time:.2f}")
-            action = np.array([[0]])  # Example action
-            self.state = self.dynamics(pendulum.state, action)
+            self.state = output['x_t'][t]
+            self.action = output['u_t'][t]
+            self.next_state = self.dynamics(self.state, self.action)
             self.history.append(self.state.flatten())
             self.time += self.data.dt
         self.plot_results()
     
+    # Reset the simulation to the initial state
     def reset(self):
         self.state = self.data.initial_state
         self.time = 0
         self.history = []
 
+    # Plot the results of the simulation
     def plot_results(self):
         # Plot the results
         history = np.array(self.history)
@@ -93,12 +130,103 @@ class Pendulum:
         plt.grid()
         plt.show()
 
-    def LQRBackwardRecursion(self):
-        pass
+    def LQRBackwardForwardRecursion(self, Ad, Bd):
+        x1, x2, x3, x4 = sp.symbols('x1 x2 x3 x4')
+        u = sp.symbols('u1')
+        state = sp.Matrix([x1, x2, x3, x4])
+        action = sp.Matrix([u])
+        
+        A = sp.Matrix(Ad)
+        B = sp.Matrix(Bd)
+        Q = sp.Matrix(self.Q)
+        R = sp.Matrix(self.R)
 
-    def LQRForwardPassRecursion(self):
-        pass
+        f_x_u =  A @ state + B @ action
+        cost_func = state.T @ Q @ state + action.T @ R @ action
 
+        # F_t = del x_t f(x_t,u_t), f_t = del u_t f(x_t,u_t)
+        F_t = f_x_u.jacobian(state)
+        f_t = f_x_u.jacobian(action)
+
+        # C_t = doubledel x_t cost_func, c_t = del u_t cost_func
+        c_t = cost_func.jacobian(state)
+        C_t = cost_func.jacobian(action)
+
+        self.state = self.initial_state
+        self.action = self.desired_action
+
+        # Delta x_t and delta u_t
+        delta_x_t = self.state - self.desired_state
+        delta_u_t = self.action - self.desired_action
+
+        # Define V_t+1 and v_t+1
+        V_t_plus_1 = self.Q @ (self.state - self.desired_state)
+        v_t_plus_1 = self.R @ (self.action - self.desired_action)
+
+        # Run the backward recursion
+        for t in range(self.time_steps - 1, -1, -1):
+            self.output['time'].append(self.time)
+
+            # Calculate Q_t,q_t
+            print(f"shape of F_t transpose: {F_t.T.shape}")
+            print(f"shape of V_t_plus_1: {V_t_plus_1.shape}")
+            print(f"shape of f_t: {f_t.shape}")
+            print(f"shape of v_t_plus_1: {v_t_plus_1.shape}")
+            Q_t = C_t + F_t.T @ V_t_plus_1 @ F_t
+            q_t = c_t + F_t.T @ V_t_plus_1 @ f_t + F_t.T @ v_t_plus_1
+
+            # Calculate Q(x_t,u_t)
+            vecstate = self.state - self.desired_state
+            vecaction = self.action - self.desired_action
+            stacked_vec = np.vstack((vecstate, vecaction))
+            Qofx_t_u_t = 0.5 * (stacked_vec).T @ Q_t @ (stacked_vec) + vecaction.T @ q_t
+
+            # Calculate u_t
+            u_t = np.argmin(Qofx_t_u_t)
+            self.action = u_t
+
+            # Calculate Quu and Qux, Qxu and Qxx
+            Quu = Q_t[4:5, 4:5]
+            Qux = Q_t[0:4, 4:5]
+            Qxu = Q_t[4:5, 0:4]
+            Qxx = Q_t[0:4, 0:4]
+            qx = q_t[0:4]
+            qu = q_t[4:5]
+
+            # Calculate K_t and k_t]
+            K_t = -np.linalg.inv(Quu) @ Qux
+            k_t = -np.linalg.inv(Quu) @ qu
+
+            # Calculate V_t and v_t
+            V_t = Qxx + Qxu @ K_t + K_t.T @ Qux + K_t.T @ Quu @ K_t
+            v_t = qx + Qxu @ k_t + K_t.T @ qu + K_t.T @ Quu @ k_t
+
+            # Calculate Vofx_t 
+            Vofx_t = 0.5 * self.state.T @ V_t @ self.state + self.state.T @ v_t
+
+            self.time -= self.data.dt
+
+            # Store the results
+            self.output['Q_t'].append(Q_t)
+            self.output['q_t'].append(q_t)
+            self.output['Qofx_t_u_t'].append(Qofx_t_u_t)
+            self.output['u_t'].append(u_t)
+            self.output['K_t'].append(K_t)
+            self.output['k_t'].append(k_t)
+            self.output['V_t'].append(V_t)
+            self.output['v_t'].append(v_t)
+            self.output['Vofx_t'].append(Vofx_t)
+        
+        # Run the forward recursion
+        self.state = self.desired_state
+        for t in range(self.time_steps):
+            u_t = self.output['K_t'][t] @ self.state + self.output['k_t'][t]
+            self.action = u_t
+            self.output['u_t'][t] = u_t
+            self.time += self.data.dt
+            self.history.append(self.state.flatten())
+
+        return self.output, self.history
 
 if __name__ == "__main__":
     data = simData()
@@ -114,7 +242,7 @@ if __name__ == "__main__":
         print("Dynamics:", pendulum.dynamics(pendulum.state, np.array([[0]])))  # Example action
         print("Pendulum simulation complete.")
 
-    pendulum.LQRBackwardRecursion()
-    pendulum.LQRForwardPassRecursion()
+    output, history = pendulum.LQRBackwardForwardRecursion(A_d, B_d)
     
-    pendulum.simulate()  # Run the simulation
+    pendulum.simulate(dynamics="linear", output=output)
+    #pendulum.simulate(dynamics="non-linear", output=output)
